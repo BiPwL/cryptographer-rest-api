386bb6945bf976a6fedde2f87e3d6d38026ed990
BiPwL <asmaev.daniil@mail.ru>
2022-11-06 03:17:50 +0300

    refactored code

---
+++ internal/app/store/teststore/store.go
@@ -0,0 +1,28 @@
+package teststore
+
+import (
+	"github.com/BiPwL/cryptographer-rest-api/internal/app/model"
+	"github.com/BiPwL/cryptographer-rest-api/internal/app/store"
+)
+
+type Store struct {
+	userRepository *UserRepository
+}
+
+func New() *Store {
+	return &Store{}
+}
+
+
+func (s *Store) User() store.UserRepository {
+	if s.userRepository != nil {
+		return s.userRepository
+	}
+
+	s.userRepository = &UserRepository{
+		store: s,
+		users: make(map[string]*model.User),
+	}
+
+	return s.userRepository
+}
